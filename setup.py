b77cfd559859e29f98593e1a2462150ad2a90f18
Graham Krizek <graham@krizek.io>
2019-01-11 12:35:00 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from ghi import __version__
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name="ghi",
+    version=__version__,
+    description="GitHub IRC Notification Service",
+    long_description=readme(),
+    author="Graham Krizek",
+    author_email="graham@krizek.io",
+    url="https://github.com/gkrizek/ghi",
+    install_requires=[
+        "PyYAML",
+        "tornado"
+    ]
+)
