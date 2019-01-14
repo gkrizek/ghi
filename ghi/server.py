27cf6e2c2c435f8e893e4dffebd9d855281e8056
Graham Krizek <graham@krizek.io>
2019-01-14 10:23:26 -0600

    Add a --quick-response option to the server

--- ghi/server.py
+++ ghi/server.py
@@ -13,12 +13,16 @@ logging.basicConfig(
 )
 
 
-def GetPort():
+def GetArgs():
     # Allow a configurable port
     parser = argparse.ArgumentParser()
-    parser.add_argument("--port", dest="port", type=int, default=8888)
+    parser.add_argument("--port", dest="port", help="Port to listen on", type=int, default=8888)
+    parser.add_argument('--quick-response', dest='quickresponse', help="Reply to client immediately, then process event", action='store_true')
     args = parser.parse_args()
-    return args.port
+    return {
+        "port": args.port,
+        "response": args.quickresponse
+    }
 
 
 def CreatePayload(method, path, body, headers):
@@ -57,11 +61,22 @@ class MainHandler(tornado.web.RequestHandler):
             body=body,
             headers=headers
         )
-        # Invoke the application
-        requestResult = handler(payload)
 
-        self.set_status(requestResult["statusCode"])
-        self.finish(requestResult["body"])
+        # Reply to the client immediately, then process event
+        quickResponse = GetArgs()['response']
+        if quickResponse:
+            # Return first, then execute function
+            self.set_status(200)
+            self.finish('{"success": true,"message":"Request received."}')
+            
+            handler(payload)
+        
+        else:
+            # Invoke the application
+            requestResult = handler(payload)
+
+            self.set_status(requestResult["statusCode"])
+            self.finish(requestResult["body"])
 
 
     def get(self):
@@ -75,10 +90,10 @@ def application():
 
 if __name__ == "__main__":
     try:
-        port = GetPort()
+        port = GetArgs()['port']
         app = application()
         app.listen(port)
         logging.info("server listening on %s" % port)
         tornado.ioloop.IOLoop.current().start()
-    except (KeyboardInterrupt, SystemExit):
+    except KeyboardInterrupt:
         logging.info("server is shutting down")
